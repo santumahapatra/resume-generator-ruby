96fdb51d8852ab952e5201cb2116f35b1ad98177
Santu Mahapatra <santu.m@multunus.com>
2014-10-06 12:26:10 +0530

    Committing code and tests for main files. Not working.

---
+++ lib/main.rb
@@ -0,0 +1,1 @@
+puts "Welcome to the Awesome Resume Generator"
---
+++ spec/lib/main_spec.rb
@@ -0,0 +1,6 @@
+require "spec_helper"
+require "main.rb"
+
+describe "Running the main.rb file" do
+  #it { expect(STDOUT).to have_received(:puts).with("Welcome to the Awesome Resume Generator") }
+end
