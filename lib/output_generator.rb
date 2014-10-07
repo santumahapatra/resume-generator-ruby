3575fb581a613dc70f2c59a68b80a69426d811e4
Santu Mahapatra <santu.m@multunus.com>
2014-10-07 18:24:53 +0530

    OutputGenerator class to call the exporters for making the resumes.

--- lib/output_generator.rb
+++ lib/output_generator.rb
@@ -1,3 +1,11 @@
 # Generates output file
 class OutputGenerator
+  def execute(resume, file_type)
+    target = find_target_class(file_type)
+    target.new.export(resume) if target != nil
+  end
+
+  def find_target_class(file_type)
+    FileExporter.descendants.detect { |klass| klass.respond_to file_type }
+  end
 end
--- spec/lib/output_generator_spec.rb
+++ spec/lib/output_generator_spec.rb
@@ -7,5 +7,39 @@ describe OutputGenerator do
   let(:resume) { "name,Zombie\nage,5" }
 
   describe '#execute' do
+
+    after(:all) do
+      file_array = Dir['resume_*.*']
+      file_array.each { |f| File.delete(f) }
+    end
+
+    it 'creates resume in csv format' do
+      output_generator.execute(resume, 'csv')
+      expect(Dir['resume_*.csv'].empty?).to eq(false)
+    end
+
+    it 'creates resume in pdf format' do
+      output_generator.execute(resume, 'pdf')
+      expect(Dir['resume_*.pdf'].empty?).to eq(false)
+    end
+
+    it 'does not create resume in lion format' do
+      output_generator.execute(resume, 'lion')
+      expect(Dir['resume_*.lion'].empty?).to eq(true)
+    end
+  end
+
+  describe '#find_target_class' do
+    it "should return CsvExporter on input 'csv'" do
+      expect(output_generator.find_target_class('csv')).to eq(CsvExporter)
+    end
+
+    it "should return PdfExporter on input 'pdf'" do
+      expect(output_generator.find_target_class('pdf')).to eq(PdfExporter)
+    end
+
+    it "should return nil on input 'lion'" do
+      expect(output_generator.find_target_class('lion')).to eq(nil)
+    end
   end
 end
