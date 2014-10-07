8a6f4b98f6eba523f274186bbbee482d4bceec81
Santu Mahapatra <santu.m@multunus.com>
2014-10-07 15:11:25 +0530

    Removing test for matching contents inside csv file.

--- spec/lib/person_spec.rb
+++ spec/lib/person_spec.rb
@@ -28,19 +28,4 @@ RSpec.describe Person do
       expect(dummy_person.create_resume).to eq(expected_output)
     end
   end
-  # describe "output file" do
-  #   it 'the csv file should have name and age' do
-  #     output_hash = {}
-  #     File.open("resume_#{dummy[:name]}.csv", 'r') do |file|
-  #       file.each_line do |line|
-  #         output = line.strip.split(',')
-  #         output_hash[output[0].to_sym] = output[1]
-  #       end
-  #     end
-
-  #     output_hash.each do |key, value|
-  #       expect(value).to eq(dummy[key.to_sym].to_s)
-  #     end
-  #   end
-  # end
 end
